dad1b9b6f4ee7573896c129ed7754414b85620c1
ron <musick@wood>
2019-09-26 17:18:05 -0400

    create sam deploy bucket

--- bookShareApp/createBS.py
+++ bookShareApp/createBS.py
@@ -15,7 +15,7 @@ import logging
 
 from threading import Thread
 # https://docs.python.org/2/library/subprocess.html
-from subprocess import call, Popen, PIPE
+from subprocess import call, check_output, Popen, PIPE
 
 from packaging import version
 from datetime import datetime
@@ -61,6 +61,8 @@ def InstallAWSPermissions():
     call( "cp "+awsBSCommon.bsAuthPath+"config " + configLoc+"/config", shell=True) 
     call( "cp "+awsBSCommon.bsAuthPath+"credentials " + configLoc+"/credentials", shell=True) 
 
+    
+# XXX Validate ubuntu
 def validateConfiguration():
     logging.info("Validating local environment")
     goodConfig = True
@@ -77,24 +79,89 @@ def validateConfiguration():
         logging.warning( "nodejs does not appear to be installed" )
         goodConfig = False
     else :
-        p = Popen(['nodejs', '--version'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        o, e = p.communicate(b"")
-
+        nver = check_output(["nodejs", "--version"]).decode('utf-8')
         # XXX mv hardcoded val
-        if( version.parse( o.decode('utf-8') ) < version.parse("8.0.0") ) :
+        if( version.parse( nver ) < version.parse("8.0.0") ) :
             logging.warning( "Please update your nodejs version" )
             goodConfig = False
         
     return goodConfig
 
 def getCFStacks():
-    assert( validateConfiguration() )
-    
-    cfn = boto3.resource('cloudformation')
+    logging.info("Get AWS Cloudformation stacks")    
+
+    # explicitly set region name, even though config file should do the same
+    cfn = boto3.resource('cloudformation', region_name = awsBSCommon.bsRegion )
     for stack in cfn.stacks.all() :
         logging.info( stack.stack_name + " " + stack.stack_status + " " +  stack.last_updated_time.strftime("%m/%d/%Y -- %H:%M:%S") )
-        #print( stack.stack_name, stack.stack_status, stack.last_updated_time )
 
+
+def s3CanCreate( bucketName ) :
+    s3 = boto3.resource('s3')
+
+    exists = True
+    canCreate = False
+    status_code = '200' 
+    try:
+        s3.meta.client.head_bucket( Bucket=bucketName )
+    except ClientError as e:
+        status_code = e.response['Error']['Code']
+        if status_code == '403':  # access denied
+            exists = True
+            canCreate = False
+        if status_code == '404':  # does not exist
+            exists = False
+            canCreate = True
+    return exists, canCreate, status_code
+    
+        
+def makeDeployBucket():
+    logging.info("Make deploy bucket" )
+
+    bucketName = awsBSCommon.samDeployBucket
+    
+    exists, canCreate, status_code = s3CanCreate( bucketName )
+    assert( not ( canCreate and exists ))
+
+    if( exists and status_code != '403' ):
+        logging.info( bucketName + " already exists, no action taken." )
+    elif( status_code == '403' ) :
+        logging.info( bucketName + " already exists, access is forbidden.  Please choose another name. ")        
+    elif( canCreate ) :
+        logging.info( bucketName + " does not exist, creating.." )
+        # aws s3 mb s3://aname.samm.deploy --region us-east-1
+        s3 = boto3.resource('s3')
+        # Strange, old bug in boto3 s3 api...
+        if( awsBSCommon.bsRegion == 'us-east-1' ) :
+            s3.create_bucket(Bucket=bucketName )
+        else :
+            s3.create_bucket(Bucket=bucketName, CreateBucketConfiguration={'LocationConstraint': awsBSCommon.bsRegion })
+
+            
+        
+
+        
+"""
+
+    # The error received here is access denied, too broad.
+    # cmd = "aws s3 ls \"s3://" + awsBSCommon.samDeployBucket + "\""
+    # p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+    # o, e = p.communicate(b"")
+    # if( e is not None ) : 
+    #    logging.warning( "SAM deployment bucket does not exist.. will create it." )
+
+
+stacks = conn.describe_stacks('MyStackID')
+if len(stacks) == 1:
+    stack = stacks[0]
+else:
+    # Raise an exception or something because your stack isn't there
+
+for output in stack.outputs:
+    print('%s=%s (%s)' % (output.key, output.value, output.description))
+
+"""
+        
     
 def main( cmd ):
     #print locals()
@@ -103,10 +170,12 @@ def main( cmd ):
                         handlers= [logging.FileHandler(filename='bookShare.log'),logging.StreamHandler()],
                         level=logging.INFO)
 
-    # XXX Set up like a makefile, almost..  Create calls x,y,z.  Destroy calls a.  Report calls e,f,g.
+    assert( validateConfiguration() )
 
-    logging.info("Get AWS stacks")
-    logging.info("Read parameters")         # names for AWS resources
+    if( cmd == "" ) :
+        getCFStacks()
+        makeDeployBucket()
+    
     logging.info("Check SAM Deploy Bucket")
     logging.info("Create S3 static web")
     logging.info("Create Cognito auth")
@@ -116,8 +185,7 @@ def main( cmd ):
     logging.info("Get insights AWS")
     logging.info("Get insights Github")
 
-    if( cmd == "validateConfiguration" ) :
-        assert( validateConfiguration() )
+    if( cmd == "validateConfiguration" or cmd == "") :
         logging.info( "finished...exiting" )
         return 
     
@@ -133,7 +201,8 @@ if __name__ == "__main__":
     # print locals()   
     # print sys.argv
     if len(sys.argv) < 2:
-        raise SyntaxError("Insufficient arguments.")
-
-    main(sys.argv[1])
+        main("")
+        #raise SyntaxError("Insufficient arguments.")
+    else:
+        main(sys.argv[1])
 
